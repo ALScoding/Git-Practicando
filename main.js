87b75021e4cdb7176b6a8ab86dac2742d5f86f61
Alexander Schalk <Alexanderlschalk@gmail.com>
2020-10-05 19:26:34 -0500

    main file

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something 
-//add something
-int var = 1;
+var product
+let num1 = 1, num2 = 2
+product = num1 + num2
